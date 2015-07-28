2ae1935a02f8618b2138d1b4eebf00d7f9b20a78
DCox-Espenlaub <DCox-Espenlaub@dcox-espenlaub.local>
2015-07-28 11:07:19 -0700

    completed servo-wave task

---
+++ servo-wave.js
@@ -0,0 +1,24 @@
+// require the arduino library
+var five = require('johnny-five');
+// create the board connection
+var board = five.Board();
+
+// reset function for realigning servo
+function reset(){
+	this.stop();
+	this.center();	
+}
+
+// wait for the board's ready state
+board.on('ready', function () {
+
+	// five.Servo takes a pin number for the constructor
+	var servo = five.Servo(9);
+
+	// sweep takes an argument of degrees
+	servo.sweep(180);
+
+	// board.wait takes ms and callback
+	board.wait(3000, reset.bind(servo));
+
+});

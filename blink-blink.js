3569b0c2f93f66b66996fc7638df183f831708c2
DCox-Espenlaub <DCox-Espenlaub@dcox-espenlaub.local>
2015-07-28 10:27:07 -0700

    created blink-blink instance

--- blink-blink.js
+++ blink-blink.js
@@ -1,2 +1,14 @@
+// require the arduino library
 var five = require('johnny-five');
+// create the board connection
 var board = five.Board();
+
+// wait for the board's ready state
+board.on('ready', function(){
+	// Led takes a pin number in the constructor
+	var led = new five.Led(13);
+
+	// blink the led once every second
+	led.blink(1000);
+
+});
